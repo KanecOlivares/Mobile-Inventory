466fd5c1443e4e3eaaed2a7e0ed5a39ba347fd27
kanecOtemp <kanecolivares@gmail.com>
2025-10-01 17:17:23 -0700

    Added a lot of UI manly recyclerview for the ListFragment and traversal from listFrag to addFrag.

--- app/src/main/java/com/example/mobileinventory/ui/list/ListFragment.kt
+++ app/src/main/java/com/example/mobileinventory/ui/list/ListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.mobileinventory.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +25,11 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener{
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+        return view
     }
 }
